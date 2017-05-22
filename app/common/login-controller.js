8b2bb94242b94f60606a588adc09724e30edbca9
linguafranka <linguafranka@users.noreply.github.com>
2017-05-22 13:01:31 -0700

    wip

--- app/common/login-controller.js
+++ app/common/login-controller.js
@@ -8,6 +8,14 @@ define([], function () {
     var self = this;
     console.log(">>", $stateParams);
 
+    var token = Identify3D.authToken();
+
+    // if we already have a token and request was forwarded from DAM
+    // then go to orders and sorty by DSI
+    if(token && $stateParams._dsiid) {
+      $state.go("identify3D.orders", {pageNum:1, keyword:'', createdAfter:0, createdBefore:0, filterByDsiId: $stateParams._dsiid, prescriptionStatus: 'created'}, { reload:true, location:'replace' });
+    }
+
     self.dummyPromise = null;
 
     self.loginForm = {};
